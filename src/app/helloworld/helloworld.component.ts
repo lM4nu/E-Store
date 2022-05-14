90f0f818fb60b305d6395d90557a61e091fd661f
M4nu <manuolakeasesosa@gmail.com>
2022-05-13 21:42:52 -0300

    Nav bar

--- src/app/helloworld/helloworld.component.ts
+++ src/app/helloworld/helloworld.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'helloworld',
-  template: ` <h2>Hello World</h2> 
-  <p>This is my first component!</p> `
-  //templateUrl: './helloworld.component.html',
-  //styleUrls: ['./helloworld.component.css']
+  templateUrl: './helloworld.component.html',
+  styleUrls: ['./helloworld.component.css']
 })
 export class HelloworldComponent implements OnInit {
 
