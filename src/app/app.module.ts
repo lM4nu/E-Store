3c0d2a07a39d77decc494904cb5c65dcf3b6d5ae
M4nu <manuolakeasesosa@gmail.com>
2022-05-16 21:14:49 -0300

    Grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PanelComponent } from './panel/panel.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PanelComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
