a420b52a6aa851037d37fef683b84e32391b9210
M4nu <manuolakeasesosa@gmail.com>
2022-05-20 19:54:06 -0300

    ngFor para generar items

---
+++ src/app/mock-products.ts
@@ -0,0 +1,11 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+	{ id: 0, name: 'Televisor', price: 300},
+	{ id: 1, name: 'Camara', price: 200},
+	{ id: 2, name: 'Microondas', price: 100},
+	{ id: 3, name: 'Heladera', price: 100},
+	{ id: 4, name: 'Parlante', price: 100},
+	{ id: 5, name: 'Computadora', price: 100},
+
+]
