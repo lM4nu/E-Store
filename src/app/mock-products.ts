f8fe3d5adfb015863ed54ea448aaef12251882e9
M4nu <manuolakeasesosa@gmail.com>
2022-05-20 21:03:24 -0300

    Add Styling

--- src/app/mock-products.ts
+++ src/app/mock-products.ts
@@ -4,8 +4,11 @@ export const PRODUCTS: Product[] = [
 	{ id: 0, name: 'Televisor', price: 300},
 	{ id: 1, name: 'Camara', price: 200},
 	{ id: 2, name: 'Microondas', price: 100},
-	{ id: 3, name: 'Heladera', price: 100},
-	{ id: 4, name: 'Parlante', price: 100},
-	{ id: 5, name: 'Computadora', price: 100},
+	{ id: 3, name: 'Heladera', price: 900},
+	{ id: 4, name: 'Licuadora', price: 400},
+	{ id: 5, name: 'Parlante', price: 150},
+	{ id: 6, name: 'Computadora', price: 400},
+	{ id: 7, name: 'Auto', price: 3400},
+	{ id: 8, name: 'Moto', price: 1000},
 
 ]
