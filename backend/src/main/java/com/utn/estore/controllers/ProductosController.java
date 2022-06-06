af4782028cfd3a688690c17a79e82c4e863d3581
M4nu <manuolakeasesosa@gmail.com>
2022-06-06 20:32:14 -0300

    added delete to the api

--- backend/src/main/java/com/utn/estore/controllers/ProductosController.java
+++ backend/src/main/java/com/utn/estore/controllers/ProductosController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller // This means that this class is a Controller
 @RequestMapping(path = "/productos") // This means URL's start with /demo (after Application path)
@@ -33,6 +34,16 @@ public class ProductosController {
         productosRepository.save(n);
         return "Saved";
     }
+
+    @GetMapping("/delete/{id}") // Map ONLY POST Requests
+    public @ResponseBody String deleteUserById(@PathVariable Integer id) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        productosRepository.deleteById(id);
+        return "Deleted";
+    }
+
 
     @GetMapping(path = "/all")
     public @ResponseBody Iterable<Productos> getAllUsers() {
