74a8903d6293f6ac4b1cf552c8b0124d48506ec0
M4nu <manuolakeasesosa@gmail.com>
2022-09-26 21:45:30 -0300

    fix typo

--- backend/src/main/java/com/utn/estore/repositories/CarritosRepository.java
+++ backend/src/main/java/com/utn/estore/repositories/CarritosRepository.java
@@ -1,12 +1,12 @@
 package com.utn.estore.repositories;
 
-import com.utn.estore.models.Carrtios;
+import com.utn.estore.models.Carritos;
 
 import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface CarrtiosRepository extends CrudRepository<Carrtios, Integer> {
+public interface CarritosRepository extends CrudRepository<Carritos, Integer> {
 
 }
