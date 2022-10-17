62fc42eeb5f08fa37fe3e81a09cb61a41c5cfc8b
M4nu <manuolakeasesosa@gmail.com>
2022-10-17 20:34:50 -0300

    formatting

--- frontend/src/app/components/cart/cart.component.ts
+++ frontend/src/app/components/cart/cart.component.ts
@@ -5,39 +5,39 @@ import {LocalStorageService} from '../services/localstorage.service';
 import {ProductsService} from '../services/products.service';
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+	selector: 'app-cart',
+	templateUrl: './cart.component.html',
+	styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
 
-  constructor(private localStorageService: LocalStorageService,
-	      private cartService: CartService, private router: Router,
-	      private productsService: ProductsService) { }
+	constructor(private localStorageService: LocalStorageService,
+      private cartService: CartService, private router: Router,
+      private productsService: ProductsService) { }
 
-  cartData?:any
+	cartData?:any;
 
-  ngOnInit(): void {
-	  const token = this.localStorageService.getToken();
-	  if(this.localStorageService.isLogged()){
-    		this.cartService.getCarrito(token)
-		.subscribe(
-			(res:any)=> {
-				this.cartData = res.carritoContent;
-				this.cartData.forEach( (item:any, index:any) => { 
-					this.productsService.getProduct(item.productoid)
-					.subscribe(
-						(res) => {
-							this.cartData[index].details = res;
-						},(err) => {
-							console.log(err);
-						})
-				});
-			}, 
-			(err) => {console.log(err)})
-	  }else{
-		  this.router.navigate(['/login']);
-	  }
-  }
+	ngOnInit(): void {
+		const token = this.localStorageService.getToken();
+		if(this.localStorageService.isLogged()){
+			this.cartService.getCarrito(token)
+				.subscribe(
+					(res:any)=> {
+						this.cartData = res.carritoContent;
+						this.cartData.forEach( (item:any, index:any) => { 
+							this.productsService.getProduct(item.productoid)
+								.subscribe(
+									(res) => {
+										this.cartData[index].details = res;
+									},(err) => {
+										console.log(err);
+									});
+						});
+					}, 
+					(err) => {console.log(err);});
+		}else{
+			this.router.navigate(['/login']);
+		}
+	}
 
 }
