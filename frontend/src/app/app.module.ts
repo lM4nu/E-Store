727740e1fc6e33dbff5c36d6882c71e88fb11982
nazareno <nazareno_cufre@hotmail.com>
2022-10-26 20:59:45 -0300

    footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { CartItemComponent } from './components/cart/cart-item/cart-item.component';
+import { AdminpanelComponent } from './components/adminpanel/adminpanel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,8 @@ import { CartItemComponent } from './components/cart/cart-item/cart-item.compone
     HomeComponent,
     LoginComponent,
     CartItemComponent,
+    AdminpanelComponent,
+    FooterComponent,
   ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
